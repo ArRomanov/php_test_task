a635d713f62c2fe36f323954806b400b7fe8ebf0
Artem Romanov <romanov-artyom@bk.ru>
2020-05-17 02:11:39 +0300

    User data test serialization are ready

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -22,35 +22,34 @@ class UserTest extends \Codeception\Test\Unit
     }
 
     /**
-     * Generating data for testAddingRepos method
+     * Generating different repo names
      *
      * @return array
      */
-    public function getRepoNamesAndCountedRating()
+    public function getRepoNames()
     {
         return array(
-            array('app\models\GithubRepo', 22.0),
-            array('app\models\GitlabRepo', 16.5),
-            array('app\models\BitbucketRepo', 22.0)
+            array('app\models\GithubRepo'),
+            array('app\models\GitlabRepo'),
+            array('app\models\BitbucketRepo')
         );
     }
 
     /**
      * Test case for adding repo models to user model (successful)
      *
-     * @dataProvider getRepoNamesAndCountedRating
+     * @dataProvider getRepoNames
      * @param string $repoClassName
-     * @param float $expectedCountedRating - посчитанный рейтинг двух репозиториев (отличается у платформ)
      * @return void
      */
-    public function testAddingReposSucceed(string $repoClassName, float $expectedCountedRating)
+    public function testAddingReposSucceed(string $repoClassName)
     {
         $greatRatingTestRepo = new $repoClassName('greatRatingTestRepo', 10, 20, 30);
         $littleRatingTestRepo = new $repoClassName('littleRatingTestRepo', 1, 2, 3);
         $this->testUserModel->addRepos(array($greatRatingTestRepo, $littleRatingTestRepo));
         $data = $this->testUserModel->getData();
         $this->assertEquals(
-            [$expectedCountedRating, 'greatRatingTestRepo', 'littleRatingTestRepo'],
+            [$this->testUserModel->getTotalRating(), 'greatRatingTestRepo', 'littleRatingTestRepo'],
             [$data['total-rating'], $data['repo'][0]['name'], $data['repo'][1]['name']]
         );
     }
@@ -69,18 +68,17 @@ class UserTest extends \Codeception\Test\Unit
     /**
      * Test case for counting total user rating (with repos)
      *
-     * @dataProvider getRepoNamesAndCountedRating
+     * @dataProvider getRepoNames
      * @param string $repoClassName
-     * @param float $expectedCountedRating - посчитанный рейтинг двух репозиториев (отличается у платформ)
      * @return void
      */
-    public function testTotalRatingCountWithRepos(string $repoClassName, float $expectedCountedRating)
+    public function testTotalRatingCountWithRepos(string $repoClassName)
     {
         $firstTestRepo = new $repoClassName('firstTestRepo', 10, 20, 30);
         $secondTestRepo = new $repoClassName('secondTestRepo', 1, 2, 3);
         $this->testUserModel->addRepos(array($firstTestRepo, $secondTestRepo));
         $actualRating = $this->testUserModel->getTotalRating();
-        $this->assertEquals($expectedCountedRating, $actualRating);
+        $this->assertEquals($this->testUserModel->getTotalRating(), $actualRating);
     }
 
     /**
@@ -95,15 +93,93 @@ class UserTest extends \Codeception\Test\Unit
     }
 
     /**
-     * Test case for user model data serialization
+     * Test case for user model data serialization without repo watcher-count
      *
      * @return void
      */
-    public function testData()
+    public function testDataWithoutWatcherCount()
     {
-        /**
-         * @todo IMPLEMENT THIS
-         */
+        $firstTestRepo = new models\GitlabRepo('firstTestRepo', 10, 20);
+        $secondTestRepo = new models\GitlabRepo('secondTestRepo', 1, 2);
+        $this->testUserModel->addRepos(array($firstTestRepo, $secondTestRepo));
+        $data = $this->testUserModel->getData();
+        $this->assertEquals(
+            [
+                $this->testUserModel->getName(),
+                $this->testUserModel->getPlatform(),
+                $this->testUserModel->getTotalRating(),
+                [],
+                ['name' => 'firstTestRepo', 'fork-count' => 10, 'start-count' => 20, 'rating' => $firstTestRepo->getRating()],
+                ['name' => 'secondTestRepo', 'fork-count' => 1, 'start-count' => 2, 'rating' => $secondTestRepo->getRating()]
+            ],
+            [
+                $data['name'],
+                $data['platform'],
+                $data['total-rating'],
+                $data['repos'],
+                $data['repo'][0],
+                $data['repo'][1]]
+        );
+    }
+
+    /**
+     * Test case for user model data serialization with watcher-count and star-count
+     *
+     * @return void
+     */
+    public function testDataWithWatcherCountAndStarCount()
+    {
+        $firstTestRepo = new models\GithubRepo('firstTestRepo', 10, 20, 30);
+        $secondTestRepo = new models\GithubRepo('secondTestRepo', 1, 2, 3);
+        $this->testUserModel->addRepos(array($firstTestRepo, $secondTestRepo));
+        $data = $this->testUserModel->getData();
+        $this->assertEquals(
+            [
+                $this->testUserModel->getName(),
+                $this->testUserModel->getPlatform(),
+                $this->testUserModel->getTotalRating(),
+                [],
+                ['name' => 'firstTestRepo', 'fork-count' => 10, 'start-count' => 20, 'watcher-count' => 30, 'rating' => $firstTestRepo->getRating()],
+                ['name' => 'secondTestRepo', 'fork-count' => 1, 'start-count' => 2, 'watcher-count' => 3, 'rating' => $secondTestRepo->getRating()]
+            ],
+            [
+                $data['name'],
+                $data['platform'],
+                $data['total-rating'],
+                $data['repos'],
+                $data['repo'][0],
+                $data['repo'][1]]
+        );
+    }
+
+    /**
+     * Test case for user model data serialization without star-count
+     *
+     * @return void
+     */
+    public function testDataWithoutStarCount()
+    {
+        $firstTestRepo = new models\BitbucketRepo('firstTestRepo', 10, 30);
+        $secondTestRepo = new models\BitbucketRepo('secondTestRepo', 1, 3);
+        $this->testUserModel->addRepos(array($firstTestRepo, $secondTestRepo));
+        $data = $this->testUserModel->getData();
+        $this->assertEquals(
+            [
+                $this->testUserModel->getName(),
+                $this->testUserModel->getPlatform(),
+                $this->testUserModel->getTotalRating(),
+                [],
+                ['name' => 'firstTestRepo', 'fork-count' => 10, 'watcher-count' => 30, 'rating' => $firstTestRepo->getRating()],
+                ['name' => 'secondTestRepo', 'fork-count' => 1, 'watcher-count' => 3, 'rating' => $secondTestRepo->getRating()]
+            ],
+            [
+                $data['name'],
+                $data['platform'],
+                $data['total-rating'],
+                $data['repos'],
+                $data['repo'][0],
+                $data['repo'][1]]
+        );
     }
 
     /**
