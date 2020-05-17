06280d289c148c50c29f25e0b605bfac63b8291b
Artem Romanov <romanov-artyom@bk.ru>
2020-05-17 18:07:28 +0300

    Tests for bitbucket data serialization are ready

--- tests/unit/BitbucketRepoTest.php
+++ tests/unit/BitbucketRepoTest.php
@@ -6,7 +6,7 @@ use app\models;
 
 /**
  * BitbucketRepoTest contains test cases for bitbucket repo model
- * 
+ *
  * IMPORTANT NOTE:
  * All test cases down below must be implemented
  * You can add new test cases on your own
@@ -37,25 +37,48 @@ class BitbucketRepoTest extends \Codeception\Test\Unit
      * @dataProvider getForkCountWatcherCountAndRating
      * @param int $forkCount
      * @param int $watcherCount
-     * @param int $expectedRating
+     * @param float $expectedRating
      * @return void
      */
-    public function testRatingCount(int $forkCount, int $watcherCount, float $expectedRating )
+    public function testRatingCount(int $forkCount, int $watcherCount, float $expectedRating)
     {
         $bitbucketTestRepo = new models\BitbucketRepo('bitbucketTestRepo', $forkCount, $watcherCount);
         $this->assertEquals($expectedRating, $bitbucketTestRepo->getRating());
     }
 
     /**
+     * Generating all bitbucket repo data
+     *
+     * @return array
+     */
+    public function getAllBitbucketRepoData()
+    {
+        return array(
+            array('repoName', 0, 0, 0.0),
+            array('ИмяРепозитория', 0, 1, 0.5),
+            array('!@#$%^&*', 1, 1, 1.5),
+            array('repo-name', 1, 0, 1.0),
+            array('repo.name', 10, 20, 20.0)
+        );
+    }
+
+    /**
      * Test case for repo model data serialization
      *
+     * @dataProvider getAllBitbucketRepoData
+     * @param string $repoName
+     * @param int $forkCount
+     * @param int $watcherCount
+     * @param float $expectedRating
      * @return void
      */
-    public function testData()
+    public function testData(string $repoName, int $forkCount, int $watcherCount, float $expectedRating)
     {
-        /**
-         * @todo IMPLEMENT THIS
-         */
+        $bitbucketTestRepo = new models\BitbucketRepo($repoName, $forkCount, $watcherCount);
+        $this->assertEquals(
+            ['name' => $repoName, 'fork-count' => $forkCount, 'watcher-count' => $watcherCount, 'rating' => $expectedRating,],
+            $bitbucketTestRepo->getData()
+        );
     }
 
     /**
